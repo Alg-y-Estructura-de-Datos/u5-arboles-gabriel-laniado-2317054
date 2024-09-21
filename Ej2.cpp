b0b2bcfcc09ac658e11f5624e313db75d774c201
Gabriellaniado <2317054@ucc.edu.ar>
2024-09-21 15:46:03 -0300

    s

--- Ej2.cpp
+++ Ej2.cpp
@@ -1,9 +1,52 @@
 #include <iostream>
+#include "Arbol/ArbolBinario.h"
 using namespace std;
 
 int main() {
 
-    cout << "Ejercicio N° 2" << endl;
+    ArbolBinario<int> inventario;
+
+    inventario.put(4001);
+    inventario.put(2002);
+    inventario.put(6003);
+    inventario.put(1500);
+    inventario.put(3500);
+    inventario.put(5000);
+    inventario.put(7000);
+
+    try{
+        int producto = inventario.search(2002);
+        inventario.remove(producto);
+         cout << "El producto "<<producto<<" se elimino del inventario."<<endl;
+    } catch (int e){
+        if (e==404){
+            cout<<"No se encontro el producto"<<endl;
+        }
+    }
+
+    try{
+        int producto = inventario.search(6003);
+        inventario.remove(producto);
+        cout << "El producto "<<producto<<" se elimino del inventario."<<endl;
+    } catch (int e){
+        if (e==404){
+            cout<<"No se encontro el producto"<<endl;
+        }
+    }
+
+    inventario.put(2500);
+
+    inventario.print();
+
+    try{
+        int producto = inventario.search(4001);
+        cout << "El producto "<<producto<<" esta en el inventario."<<endl;
+    } catch (int e){
+        if (e==404){
+            cout<<"No se encontro el producto " <<endl;
+        }
+    }
+
 
     return 0;
 }
--- Ej3.cpp
+++ Ej3.cpp
@@ -1,9 +1,47 @@
 #include <iostream>
+#include "Arbol/ArbolBinario.h"
 using namespace std;
 
-int main() {
+int main()
+{
 
-    cout << "Ejercicio N° 3" << endl;
+    ArbolBinario<int> arbol;
 
+    arbol.put(500);
+    arbol.put(250);
+    arbol.put(750);
+    arbol.put(100);
+    arbol.put(300);
+    arbol.put(600);
+    arbol.put(800);
+
+    try
+    {
+        int producto = arbol.search(250);
+        arbol.remove(producto);
+        cout << "El integrante " << producto << " se elimino." << endl;
+    }
+    catch (int e)
+    {
+        if (e == 404)
+        {
+            cout << "No se encontro el miembro" << endl;
+        }
+    }
+
+    if (arbol.search(300)==300) {
+        cout <<"El integrante 300 fue encontrado"<<endl;
+    } else {
+        cout<<"No se encontro al miembro 300."<<endl;
+    }
+
+    if (arbol.search(750)==750) {
+        cout <<"El integrante 750 fue encontrado"<<endl;
+    } else {
+        cout<<"No se encontro al miembro 750."<<endl;
+    }
+    
+    arbol.print();
+    arbol.postorder();
     return 0;
 }
